6e92a12f11ade316bbed870f84670dc3880f0e2f
Danilo Zeihs <d.zeihs@tarent.de>
2017-02-15 17:54:15 +0100

    add small simple data für gui

--- src/main/java/boot/controller/ListUiController.java
+++ src/main/java/boot/controller/ListUiController.java
@@ -5,6 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -17,8 +22,11 @@ public class ListUiController {
     public EntityRepository repo;
 
     @RequestMapping(method = RequestMethod.GET, value = "/ui")
-    public String getAll() {
-        return "index";
+    public ModelAndView getAll() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("time", new Date());
+        map.put("message", "Hello World.");
+        return (new ModelAndView( "index", map));
     }
 
 }
